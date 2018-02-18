cca20e6006e7ea560ac256812d6590d7f59cea53
Cefo14 <cefo_edd@hotmail.com>
2018-02-18 02:01:27 -0600

    prettify code
    I was bored

--- routes/streams.js
+++ routes/streams.js
@@ -1,49 +1,46 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
 
-var mime = require('mime');
-var onFinished = require('on-finished');
-var torrentStream = require('torrent-stream');
+const express = require('express')
+const router = express.Router()
+const mime = require('mime')
+const torrentStream = require('torrent-stream')
 
 /* GET make streaming */
 router.get('/stream/:id', function(req, res, next) 
 {
-	const engine = torrentStream(req.session.magnament);
+	const engine = torrentStream(req.session.magnament)
+	let stream
+
 	engine.on('ready', () => {
-		const index = parseInt(req.params["id"]);
-		pipe(res, engine.files[index]);
-	});
-});
+		stream ? stream.destroy() : null
+		const index = parseInt(req.params["id"])
+		let file = engine.files[index]
+		const type = mime.lookup(file.path)
+		
+		res.header('Content-Type', type)
+		res.header('Content-Length', file.length)
+		res.header('Accept-Ranges', 'bytes')
+
+		stream = file.createReadStream()
+		stream.pipe(res)
+	})
+
+	engine.on('idle', () => stream ? stream.destroy() : null)
+	engine.on('download', (data) => console.log(data))
+})
 
 /* GET return a name files ant set magnament on session*/
 router.post('/show', function(req, res, next) 
 {
-	req.session.magnament = req.body.magnament;
-	const engine = torrentStream(req.body.magnament);
-	let filesNames = [];
+	req.session.magnament = req.body.magnament
+	const engine = torrentStream(req.body.magnament)
+	let filesNames = []
 	engine.on('ready', () => {
 		engine.files.forEach((file, index) => {
 			mime.lookup(file.path) === "audio/mpeg" ? filesNames.push({ id:index, name: file.name }) : true
 		});
-		res.json(filesNames);
+		res.json(filesNames)
 	});
 });
-
-function pipe (res, file) 
-{
-	let stream;
-	const type = mime.lookup(file.path);
-	
-	res.header('Content-Type', type);
-	res.header('Content-Length', file.length);
-	res.header('Accept-Ranges', 'bytes');
-	
-	stream = file.createReadStream();
-	
-	onFinished(res, () => stream && stream.destroy());
-
-	// Make a stream
-	stream.pipe(res);
-}
 
 module.exports = router;
