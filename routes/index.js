de3b98ce3c74f9df1bb8703516be80c52aa92b3b
Jesús Castro <x51v4n@gmail.com>
2018-03-03 16:43:22 -0600

    upd: typos spoted (and fixed)
    - Fixed typo spoted.
    - Fixed 'style.css' another typo spoted.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Coffie Time' });
+  res.render('index', { title: 'Coffee Time' });
 });
 
 module.exports = router;
