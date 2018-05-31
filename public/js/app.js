d034b622b994367008a7798a6526d33e3fd4cc64
Cefo14 <cefo_edd@hotmail.com>
2018-05-31 18:58:10 -0500

    2.01

--- public/js/app.js
+++ public/js/app.js
@@ -1,85 +1,152 @@
 $(document).ready(function()
 {
 	var menu = $('#menu')
-	var audio = $('audio');
+	var audio = $('audio')
+	var li
 
-	$('#search').click(function(){
+	$('#search').click(function(event)
+	{
+		event.preventDefault()
+
 		$.ajax(
 		{
 			url: '/streams/search',
 			type: 'POST',
-			data : { search : $('#name').val() }
+			data : { search : $('#name').val() },
+			dataType:'json',
+			beforeSend : load
 		})
 			.done(function (values) 
 			{
-				menu.empty()
-
-				values.forEach(function(value)
-				{
-					var a = $('<a class="list-group-item list-group-item-action"></a>')
-					a.text(value.name)
-					a.attr('href', value.url)
-					a.click(show)
-					menu.append(a)
-				})
-
+				unload()
+				insert(values, 'search')
 			})
 
-			.fail(function(){
-				throw "Error";
+			.fail(function()
+			{
+				unload()
+				showError('No Results')
 			})
 	})
 
-	$('#home').click(function (e) 
+	$('#home').click(function(event) 
 	{
-		menu.empty()
-    })
+		event.preventDefault()
+		clear()
+	})
+
+	$('#name').keyup(function(event)
+	{
+		if(event.keyCode == 13)
+			$('#search').trigger('click')
+	})
+
+	audio.on('playing', function()
+	{
+		unload()
+	})
+
+	audio.on('waiting', function()
+	{
+		load('li')
+	})
+
+	audio.on('ended',function() 
+	{
+		li = li.next()
+		if(li)
+			li.trigger('click')
+	})
 
 	function show(event)
 	{
 		event.preventDefault()
-		menu.empty()
-
 		$.ajax(
 		{
 			url: '/streams/show',
 			type: 'POST',
-			data : { url : $(this).attr('href') }
+			data : { url : $(this).attr('href') },
+			beforeSend : load
 		})
 			.done(function (values) 
 			{
-				menu.empty()
-
-				values.forEach(function(value)
-				{
-					var a = $('<a class="list-group-item list-group-item-action"></a>')
-					a.text(value.name)
-					a.attr('id', value.id)
-					a.click(value, play)
-					menu.append(a)
-				})
-
+				unload()
+				insert(values, 'show')
 			})
 
-			.fail(function(){
-				throw "Error";
+			.fail(function()
+			{
+				unload()
+				showError('No Results')
 			})
 	}
 
 	function play(event)
 	{
 		$('.active ').removeClass('active')
-		
-		this.className += ' active'
-		audio[0].pause();
-		audio.attr('src', '/streams/stream/' + event.data.id);
-		audio[0].play();
+		li = $(this)
+		li.addClass('active')
+		audio[0].pause()
+		audio.attr('src', '/streams/stream/' + event.data.id)
+		audio[0].play()
 	}
 
-	audio.on('ended',function() {
-		var src = audio.attr('src');
-		var id = parseInt(src.split('/')[3]);
-		audio.attr('src', '/streams/stream/' + (++id));
-		audio[0].play();
-	});
+	function insert(values, type)
+	{
+		values.forEach(function(value)
+		{
+			var a = $('<a class="list-group-item list-group-item-action">')
+			a.text(value.name)
+			
+			if(type === 'search')
+			{
+				a.attr('href', value.url)
+				a.click(show)
+			}
+
+			else if(type === 'show')
+			{
+				a.attr('id', value.id)
+				a.click(value, play)
+			}
+			menu.append(a)
+		})
+	}
+
+	function clear()
+	{
+		menu.empty()
+	}
+
+	function load(type)
+	{
+		if(type !== 'li')
+			clear()
+
+		var div = $('<div class="spinner">')
+		for (var i = 1; i < 6; i++) 
+		{
+			var rect = $('<div>')
+			rect.addClass('rect' + i)
+			div.append(rect)
+		}
+
+		if(type === 'li')
+			li.append(div)
+
+		else
+			menu.append(div)
+	}
+
+	function unload() 
+	{
+		$('.spinner').remove()
+	}
+
+	function showError(text)
+	{
+		var div = $('<div class="alert alert-info" role="alert">')
+		div.text(text)
+		menu.append(div)
+	}
 });
